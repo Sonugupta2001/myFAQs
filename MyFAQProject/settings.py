e5cbc55c6370ba72704babfdc0a419e271a03511
Sonu Gupta <sg6263709700@gmail.com>
2025-02-01 23:06:00 +0530

    fix: Add whitenoise

--- MyFAQProject/settings.py
+++ MyFAQProject/settings.py
@@ -214,3 +214,6 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Whitenoise settings
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
